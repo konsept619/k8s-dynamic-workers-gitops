5db5865eabed67c1ff6f7804dfe8099162f9c86f
konsept619 <94557832+konsept619@users.noreply.github.com>
2026-05-19 13:28:58 +0200

    Update: main.py - display env variables and query
    Added environment variable checks and status printing.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,39 @@
 import os
+import time
+from datetime import datetime
 import importlib
 
-def main():
+VARIABLES_TO_CHECK = [
+    "DBUSER", "DB_TYPE",
+    "BKTNAME", "LOGLEVEL", "HTTP_PROXY_MOCK", "HTTPS_PROXY_MOCK"
+]
 
+def print_status():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n--- ETL Status Check: {now} ---")
+
+    print("[Env Vars]:")
+    for var in VARIABLES_TO_CHECK:
+        value = os.getenv(var, "NOT SET")
+        print(f"  {var}: {value}")
+
+def print_query():
     etl_db_type = os.environ.get("DB_TYPE", "postgres").lower()
-    
+
     try:
         query_module = importlib.import_module(f"queries.{etl_db_type}")
         query_module.run_query()
-        
     except ModuleNotFoundError:
-        print(f"Error: No module for ETL_TYPE={etl_db_type}")
+        print(f"Error: No module named 'queries.{etl_db_type}' found.")
+    except Exception as e:
+        print(f"Error during query execution: {e}")
 
 if __name__ == "__main__":
-    main()
+    print("ETL Application Container Started.")
+    try:
+        while True:
+            print_status()
+            print_query()
+            time.sleep(120)
+    except KeyboardInterrupt:
+        print("\nETL Application Container Stopped gracefully.")
